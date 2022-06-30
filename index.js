2bdb2ad976bfef78ed799486a5b583de3720058c
saddamhosan <contact.saddamhosan.com>
2022-06-30 22:27:08 +0600

    update

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jz7u1.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -17,37 +16,54 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
-
-
 async function run() {
   try {
     await client.connect();
     const taskCollection = client.db("todo-app").collection("task");
 
     app.get("/task", async (req, res) => {
-      const result = await taskCollection.find().toArray();
+        const query = { completed:false };
+      const result = await taskCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.get("/completed", async (req, res) => {
+        const query = { completed:true };
+      const result = await taskCollection.find(query).toArray();
       res.send(result);
     });
     app.get("/task/:id", async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: ObjectId(id) };
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
       const result = await taskCollection.findOne(query);
       res.send(result);
     });
     app.put("/task/:id", async (req, res) => {
-        const id = req.params.id;
-        const task = req.body.task;
-        console.log(task);
-        const filter = { _id: ObjectId(id) };
-        const options = { upsert: true };
-        const updateDoc = {
-          $set: {
-            task,
-          },
-        };
+      const id = req.params.id;
+      const task = req.body.task;
+      console.log(task);
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          task,
+        },
+      };
       const result = await taskCollection.updateOne(filter, updateDoc, options);
       res.send(result);
     });
+
+    app.put("/completed/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          completed: true,
+        },
+      };
+      const result = await taskCollection.updateOne(query, updatedDoc);
+      res.send(result);
+    });
+
     app.post("/task", async (req, res) => {
       const doc = req.body;
       const result = await taskCollection.insertOne(doc);
